69f5d4570a944aec267e6d8cb6c2e9020e8606a4
Александр Власов <velesov7493@yandex.ru>
2021-07-29 18:31:02 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread progressTask = new Thread(() -> {
+            try {
+                for (int i = 0; i <= 100; i++) {
+                    System.out.print("\rЗагрузка: " + i + "%");
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Поток прогресса прерван!");
+            }
+        });
+        progressTask.start();
+    }
+}
