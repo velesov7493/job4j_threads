a70aa471c964abd4fca40d8c52aaba4eafec9279
Александр Власов <velesov7493@yandex.ru>
2021-07-31 17:58:19 +0300

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/concurrent/ImmutableNode.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+public class ImmutableNode<T> {
+
+    private final ImmutableNode<T> next;
+    private final T value;
+
+    public ImmutableNode(ImmutableNode<T> aNext, T aValue) {
+        next = aNext;
+        value = aValue;
+    }
+
+    public ImmutableNode<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
