18c1764f864b8679b98d5130e2931c0465c45a0e
Александр Власов <velesov7493@yandex.ru>
2021-07-31 17:01:58 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/concurrent/DclSingleton.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class DclSingleton {
+
+    private static volatile DclSingleton inst;
+
+    private DclSingleton() { }
+
+    public static DclSingleton instOf() {
+        if (inst == null) {
+            synchronized (DclSingleton.class) {
+                if (inst == null) {
+                    inst = new DclSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+}
