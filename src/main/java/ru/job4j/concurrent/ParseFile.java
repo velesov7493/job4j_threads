2f52e567b9acab5c8f12c418318892e02ca3e166
Александр Власов <velesov7493@yandex.ru>
2021-08-01 09:35:04 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -0,0 +1,35 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+
+    private final File file;
+
+    public ParseFile(File aFile) {
+        file = aFile;
+    }
+
+    private String getContentByPredicate(Predicate<Character> filter) throws IOException {
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader  br = new BufferedReader(new FileReader(file))) {
+            int data;
+            while ((data = br.read()) > 0) {
+                char ch = (char) data;
+                if (filter.test(ch)) {
+                    output.append(ch);
+                }
+            }
+        }
+        return output.toString();
+    }
+
+    public String getContent() throws IOException {
+        return getContentByPredicate((c) -> true);
+    }
+
+    public String getContentWithoutUnicode() throws IOException {
+        return getContentByPredicate((c) -> c < 0x80);
+    }
+}
---
+++ src/main/java/ru/job4j/concurrent/SaveFile.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+
+public class SaveFile {
+
+    private final File file;
+
+    public SaveFile(File aFile) {
+        file = aFile;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (PrintWriter pw =
+                new PrintWriter(
+                    new BufferedWriter(
+                        new FileWriter(file)
+        ))) {
+            pw.print(content);
+        }
+    }
+}
