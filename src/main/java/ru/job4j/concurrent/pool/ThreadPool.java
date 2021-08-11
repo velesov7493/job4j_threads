539100323458822b3abb1cc8183bb28820f1c156
Александр Власов <velesov7493@yandex.ru>
2021-08-11 13:49:01 +0300

    1. Реализовать ThreadPool [#1099 #188176]

--- src/main/java/ru/job4j/concurrent/pool/ThreadPool.java
+++ src/main/java/ru/job4j/concurrent/pool/ThreadPool.java
@@ -3,36 +3,32 @@ package ru.job4j.concurrent.pool;
 import ru.job4j.concurrent.queue.BlockingQueue;
 import ru.job4j.concurrent.queue.SimpleBlockingQueue;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-public class ThreadPool  extends Thread {
+public class ThreadPool {
 
-    private final List<Thread> threads;
+    private final List<Thread> threads = new LinkedList<>();
     private final BlockingQueue<Runnable> tasks;
-    private final int maxThreads;
 
-    public ThreadPool() {
-        maxThreads = Runtime.getRuntime().availableProcessors();
-        threads = new ArrayList<>();
-        tasks = new SimpleBlockingQueue<>(20);
-        setPriority(Thread.MIN_PRIORITY);
+    private class PoolThread extends Thread {
+
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+                Runnable task = tasks.poll();
+                task.run();
+            }
+        }
     }
 
-    @Override
-    public void run() {
-        int i = 0;
-        while (!isInterrupted()) {
-            Thread entry = threads.get(i);
-            if (entry != null && !entry.isAlive()) {
-                threads.remove(i);
-            }
-            if (threads.size() < maxThreads) {
-                entry = new Thread(tasks.poll());
-                threads.add(entry);
-                entry.start();
-            }
-            i = (i + 1) % threads.size();
+    public ThreadPool(int maxTasks) {
+        int maxThreads = Runtime.getRuntime().availableProcessors();
+        tasks = new SimpleBlockingQueue<>(maxTasks);
+        for (int i = 0; i < maxThreads; i++) {
+            PoolThread t = new PoolThread();
+            threads.add(t);
+            t.start();
         }
     }
 
